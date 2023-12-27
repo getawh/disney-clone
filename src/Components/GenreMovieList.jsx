a6f84934c9fbfa3fe032ef0d36f0098e85e872ca
getaw habtamu <getawh@gmail.com>
2023-12-27 14:03:50 -0300

    list movie b genre

--- src/Components/GenreMovieList.jsx
+++ src/Components/GenreMovieList.jsx
@@ -2,27 +2,18 @@ import React, { useState } from 'react'
 import GlobalApi from '../services/GlobalApi'
 import GenresList from '../Constants/GenresList'
 import imageBaseUrl from './Slider'
+import MovieList from './MovieList'
 
 function GenreMovieList() {
-    // const [genreMovieList, setGenreMovieList] = useState([])
-    // setGenreMovieList(GlobalApi.getMovieByGenreId(item.id))
-
-    // getMovieByGenreId = (id) => {
-    //     GlobalApi.getMovieByGenreId(id).then(resp => {
-    //         setGenreMovieList(resp.data.results)
-    //     }
-
-    //         )
-    // }
     return (
         <div>
-            {GenresList.genre.map((item,index) => index<=4 && (
-                
-                <div>
-                    <h2 className='font-bold text-16 p-8 px-8 md:px-16'>
-                    {item.name}
+            {GenresList.genre.map((item, index) => index <= 4 && (
+
+                <div className='p-8 px-8 md:px-16' >
+                    <h2 className='font-bold text-16 '>
+                        {item.name}
                     </h2>
-                    <MovieList/>
+                    <MovieList genreId={item.id} />
                 </div>
             )
 
--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import GlobalApi from '../services/GlobalApi'
 
-function MovieList() {
-  return (
-    
-    <div>MovieList</div>
-  )
+const imageBaseUrl = 'https://image.tmdb.org/t/p/original'
+
+function MovieList({ genreId }) {
+    const [genreMovieList, setGenreMovieList] = useState([])
+    // setGenreMovieList(GlobalApi.getMovieByGenreId(item.id))
+
+    useEffect(() => (
+        getMovieByGenreId()
+    ), [])
+
+    const getMovieByGenreId = () => {
+        GlobalApi.getMovieByGenreId(genreId).then(resp => {
+            console.log(resp.data.results)
+            setGenreMovieList(resp.data.results)
+        }
+
+        )
+    }
+    return (
+        <div className='flex w-full overflow-x-auto scrollbar-hide'>
+
+{        genreMovieList.map((item) => (
+
+            <img src={imageBaseUrl + item.backdrop_path} className='w-[200px] object-cover m-5 rounded-md hover:border-2 hover:scale-110 border-gray-200 transition-all duration-500 ease-in-out' />
+
+
+            ))}
+        </div>
+            )
 }
 
 export default MovieList
